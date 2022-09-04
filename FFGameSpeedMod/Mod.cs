4b7fc49bb89cfc3b417b6093ec813741a483fd10
Stefano Tenuta <stapps@outlook.it>
2022-09-04 16:32:36 +0200

    Aggiungere i file di progetto.

---
+++ FFGameSpeedMod/Mod.cs
@@ -0,0 +1,104 @@
+﻿using MelonLoader;
+using System.Collections.Generic;
+using System.Linq;
+using UnhollowerBaseLib;
+using UnityEngine;
+
+namespace FFGameSpeedMod
+{
+    public class Mod : MelonMod
+    {
+        /// <summary>
+        /// Internal name for the <see cref="TimeManager.timeScales"/> property.
+        /// </summary>
+        private const string _timeScalesPropertyName = "timeScales";
+
+        /// <summary>
+        /// Timescales to be either added or unlocked, alongside their respective <see cref="KeyCode"/> value.
+        /// Keys from 1 to 4 cover the default cases bundled with vanilla game.
+        /// </summary>
+        private readonly Dictionary<KeyCode, float> _customTimeScales = new Dictionary<KeyCode, float> {
+            { KeyCode.Alpha5, 5 },  // Default but locked
+            { KeyCode.Alpha6, 10 }, // Default but locked
+            { KeyCode.Alpha7, 15 }, // Custom
+            { KeyCode.Alpha8, 20 }, // Custom
+            { KeyCode.Alpha9, 30 }, // Custom
+            { KeyCode.Alpha0, 50 }  // Custom
+        };
+
+        /// <summary>
+        /// Prevent reloading custom time scales multiple times.
+        /// 
+        /// TODO: Find a way to run <see cref="InitTimeScales"/> only on game load using <see cref="MelonLoader"/>.
+        /// </summary>
+        private static bool _isLoaded;
+
+        /// <summary>
+        /// Reference to the <see cref="TimerManager"/> instance for the current game.
+        /// </summary>
+        private TimeManager TimeManager => GameObject.Find("GameManager")?.GetComponent<GameManager>()?.timeManager;
+
+        /// <summary>
+        /// Reference to the <see cref="TimeManager.timeScales"/> array which contains all the available multipliers.
+        /// </summary>
+        private float[] TimeScales
+        {
+            get => (Il2CppStructArray<float>)TimeManager
+                .GetType()
+                .GetProperties()
+                .FirstOrDefault(p => p.Name == _timeScalesPropertyName)
+                .GetValue(TimeManager);
+
+            set => TimeManager
+                .GetType()
+                .GetProperties()
+                .FirstOrDefault(p => p.Name == _timeScalesPropertyName)
+                .SetValue(TimeManager, (Il2CppStructArray<float>)value);
+        }
+
+        /// <summary>
+        /// Adds the custom time scales defined in <see cref="_customTimeScales"/>.
+        /// </summary>
+        private void InitTimeScales()
+        {
+            if (_isLoaded) return;
+
+            LoggerInstance.Msg($"Setting up extended timescales...");
+
+            if (TimeManager == null)
+            {
+                LoggerInstance.Msg("TimeManager is null, aborting...");
+                return;
+            }
+
+            TimeScales = TimeScales.Union(_customTimeScales.Values).ToArray();
+            _isLoaded = true;
+        }
+
+        /// <summary>
+        /// Dynamically deal with custom time scales by setting the one associated to the pressed key.
+        /// </summary>
+        public override void OnUpdate()
+        {
+            // Check if any of the managed KeyCodes is being pressed
+            for (var i=0; i<_customTimeScales.Count; i++)
+            {
+                var keyCode = _customTimeScales.Keys.ToArray()[i];
+                if (Input.GetKeyDown(keyCode))
+                {
+                    // We need to init our custom time scales
+                    InitTimeScales();
+
+                    // Now we're ready to set the time scale value based on the pressed keycode.
+                    // It's important to remember that keys from 1 to 4 are already taken!
+                    // This means that our array index will be current key + 4
+                    var timeScaleIndex = i + 4;
+                    LoggerInstance.Msg($"Setting time scale index to {timeScaleIndex} with value {TimeScales[timeScaleIndex]}");
+
+                    TimeManager.SetTimeScale((uint)timeScaleIndex, true);
+                    LoggerInstance.Msg($"New value for time scale is {TimeManager.GetTimeScale()}");
+                }
+            }
+        }
+    }
+}
---
+++ FFGameSpeedMod/Properties/AssemblyInfo.cs
@@ -0,0 +1,42 @@
+﻿using FFGameSpeedMod;
+using MelonLoader;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// MelonLoader Metadata
+[assembly: MelonInfo(typeof(Mod), "GameSpeedMod", "0.0.1", "S_T")] //MelonInfo(typeof(ClassOfMod), "Game Mod Name", "Version", "Author")
+[assembly: MelonGame("Crate Entertainment", "Farthest Frontier")] //MelonGame("Studio", "Game")]
+
+// Le informazioni generali relative a un assembly sono controllate dal seguente 
+// set di attributi. Modificare i valori di questi attributi per modificare le informazioni
+// associate a un assembly.
+[assembly: AssemblyTitle("FFGameSpeedMod")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FFGameSpeedMod")]
+[assembly: AssemblyCopyright("Copyright © S_T 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Se si imposta ComVisible su false, i tipi in questo assembly non saranno visibili
+// ai componenti COM. Se è necessario accedere a un tipo in questo assembly da
+// COM, impostare su true l'attributo ComVisible per tale tipo.
+[assembly: ComVisible(false)]
+
+// Se il progetto viene esposto a COM, il GUID seguente verrà utilizzato come ID della libreria dei tipi
+[assembly: Guid("33244ef5-211a-4976-b27d-39a751eb09ea")]
+
+// Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
+//
+//      Versione principale
+//      Versione secondaria
+//      Numero di build
+//      Revisione
+//
+// È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build
+// usando l'asterisco '*' come illustrato di seguito:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
